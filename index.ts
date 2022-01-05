ea47b8ee04b320fda3730a0c6c807c82a91d0459
Ahmed <atomcgarry@gmail.com>
2022-01-05 14:28:00 +0000

    plus one

--- index.ts
+++ index.ts
@@ -14,15 +14,28 @@
 //     return indexes
 // }
 
-
 // console.log(twoSum([3,2,4], 6))
 
 // Palendrome NUmber
 
-const isPalindrome = (x: number): boolean => {
-    let palindromeNumber: string | number = x;
-    palindromeNumber = Number(palindromeNumber.toString().split('').reverse().join(''))
-    return (palindromeNumber === x) ?  true : false
-}
+// const isPalindrome = (x: number): boolean => {
+//     let palindromeNumber: string | number = x;
+//     palindromeNumber = Number(palindromeNumber.toString().split('').reverse().join(''))
+//     return (palindromeNumber === x) ?  true : false
+// }
 
-console.log(isPalindrome(-121))
+// console.log(isPalindrome(-121))
+
+var plusOne = function(digits: number[]) {
+    for (let i = digits.length -1; i >= 0; i--) {
+        const curDigit: number = digits[i];
+        if (curDigit < 9) {
+            digits[i] = digits[i] + 1;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    return [1, ...digits];
+};
+
+console.log(plusOne([2,3,4]))
